55b1f5a5413c773aeabc5b2a84693f3fea755eb6
Ines <ines.opcoelho@gmail.com>
2017-04-19 01:47:15 +0100

    Refactor navbar active state

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,11 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+	//home button (logo)
+    $("#navHome").click(function() {
+        // remove classes from all
+        $("li").removeClass("active");
+    });
+});
