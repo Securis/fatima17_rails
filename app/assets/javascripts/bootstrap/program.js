038cca636cd940d6bd28a4f4fa8a63eac21bb591
Ines <ines.opcoelho@gmail.com>
2017-04-22 19:19:10 +0100

    program mobile version

---
+++ app/assets/javascripts/bootstrap/program.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+    $("#day1-selector").click(function(){
+    	$("#timeline-container-day1").show();
+        $("#timeline-container-day2").hide();
+        $("#day2-selector").removeClass('active');
+    });
+    $("#day2-selector").click(function(){
+    	$("#timeline-container-day2").show();
+        $("#timeline-container-day1").hide();
+        $("#day1-selector").removeClass('active');
+    });
+});
