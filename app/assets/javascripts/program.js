5d9ed30ea3c3c47c3f3c0de6528a43cf7a539fa2
Ines <ines.opcoelho@gmail.com>
2017-04-23 01:54:49 +0100

    replace day selection buttons by radio buttons

--- app/assets/javascripts/program.js
+++ app/assets/javascripts/program.js
@@ -1,13 +1,26 @@
-$(document).ready(function(){
+/*$(document).ready(function(){
     $("#day1-selector").click(function(){
     	$("#timeline-container-day1").show();
         $("#timeline-container-day2").hide();
-        $("#day2-selector").removeClass('active');
+        //$("#day2-selector").removeClass('active');
     });
     $("#day2-selector").click(function(){
     	$("#timeline-container-day2").show();
         $("#timeline-container-day1").hide();
-        $("#day1-selector").removeClass('active');
+        //$("#day1-selector").removeClass('active');
+    });
+});*/
+
+$(document).ready(function() {
+    $('input[type=radio][name=program-day-selector]').change(function() {
+        if (this.value == 'day1') {
+            $("#timeline-container-day1").show();
+            $("#timeline-container-day2").hide();
+        }
+        else if (this.value == 'day2') {
+            $("#timeline-container-day2").show();
+            $("#timeline-container-day1").hide();
+        }
     });
 });
 
