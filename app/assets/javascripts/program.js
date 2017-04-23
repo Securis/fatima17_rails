3132f5892ca454362d354f60033c3e11a5f6ef20
Ines <ines.opcoelho@gmail.com>
2017-04-23 23:58:10 +0100

    try to center events on timeline on mobile - which is a problem I'm not seeing on the pc

--- app/assets/javascripts/program.js
+++ app/assets/javascripts/program.js
@@ -1,16 +1,3 @@
-/*$(document).ready(function(){
-    $("#day1-selector").click(function(){
-    	$("#timeline-container-day1").show();
-        $("#timeline-container-day2").hide();
-        //$("#day2-selector").removeClass('active');
-    });
-    $("#day2-selector").click(function(){
-    	$("#timeline-container-day2").show();
-        $("#timeline-container-day1").hide();
-        //$("#day1-selector").removeClass('active');
-    });
-});*/
-
 document.addEventListener("turbolinks:load", function() {
     $('input[type=radio][name=program-day-selector]').change(function() {
         if (this.value == 'day1') {
