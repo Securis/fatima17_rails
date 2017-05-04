eff14a91231bb436271ace6e56fb4594bad100aa
Ines <ines.opcoelho@gmail.com>
2017-05-05 00:28:30 +0100

    put JS in application file

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,32 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+$(function() {
+    window.scrollReveal = new scrollReveal();
+});
+
+
+//LATERAL IMAGES
+$(function() {
+    $(window).resize(function() {
+        $('#width-placeholder').html($("#sidebar").width());
+    }).resize();
+});
+
+
+//PROGRAM
+document.addEventListener("turbolinks:load", function() {
+    $('input[type=radio][name=program-day-selector]').change(function() {
+        if (this.value == 'day1') {
+            $("#timeline-container-day1").show();
+            $("#timeline-container-day2").hide();
+        }
+        else if (this.value == 'day2') {
+            $("#timeline-container-day2").show();
+            $("#timeline-container-day1").hide();
+        }
+    });
+});
+
+var vr = document.registerElement('v-r');
